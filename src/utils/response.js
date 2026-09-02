ca8e3d238dfa5ec6644a291ff6cde4b88685ceb4
ab-e1 <abelghy17@gmail.com>
2026-09-03 01:06:57 +0300

    add: added the repsonse helper functions so as not make re.status() response everytime in controllers

---
+++ src/utils/response.js
@@ -0,0 +1,14 @@
+export const failure = (res, error, statusCode = 400) => {
+  res.status(statusCode).json({
+    ok: false,
+    error,
+  });
+};
+
+export const success = (res, data, statusCode = 200, token) => {
+  res.status(statusCode).json({
+    ok: true,
+    data,
+    token,
+  });
+};
