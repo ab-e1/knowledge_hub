0399fc6773c86f34a28607ba1b6b84f3711ea9f8
ab-e1 <abelghy17@gmail.com>
2026-09-03 01:07:24 +0300

    fix: changed the env name to match prima deault

--- src/config/loadEnv.js
+++ src/config/loadEnv.js
@@ -1,8 +1,8 @@
-import { dotenv } from "dotenv";
+import dotenv from "dotenv";
 
 dotenv.config();
 
 export const port = process.env.PORT;
-export const dbUri = process.env.POSTGRES_URI;
+export const dbUri = process.env.DATABASE_URL;
 export const jwtSecret = process.env.JWT_SECRET;
 export const jwtExpiresIn = process.env.JWT_EXPIRES_IN;
