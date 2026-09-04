742cb1823117afe1f515584fb6dffdb8ea83a393
ab-e1 <abelghy17@gmail.com>
2026-09-05 02:53:10 +0300

    add: added visibleinforto stipr hashed password from data as well as made success response include the two tokens

--- src/utils/response.js
+++ src/utils/response.js
@@ -5,10 +5,31 @@ export const failure = (res, error, statusCode = 400) => {
   });
 };
 
-export const success = (res, data, statusCode = 200, token) => {
+export const success = (
+  res,
+  data,
+  statusCode = 200,
+  accessToken,
+  refreshToken,
+) => {
   res.status(statusCode).json({
     ok: true,
     data,
-    token,
+    accessToken,
+    refreshToken,
   });
+};
+
+export const visibleInfo = (data) => {
+  return {
+    id: data.id,
+    name: data.name,
+    email: data.email,
+    role: data.role,
+    bio: data.bio,
+    avatarUrl: data.avatarUrl,
+    reputation: data.reputation,
+    createdAt: data.createdAt,
+    updatedAt: data.updatedAt,
+  };
 };
