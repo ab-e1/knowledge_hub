937eeb484a7b96ea57fc96d07d475da5df41f2dd
ab-e1 <abelghy17@gmail.com>
2026-09-05 23:56:07 +0300

    add: added the rest of teh auth service enpoints we added

--- src/routes/authRoute.js
+++ src/routes/authRoute.js
@@ -1,7 +1,11 @@
 import { Router } from "express";
 import * as authController from "../controllers/authController.js";
 import { validate } from "../middleware/validate.js";
-import { registerSchema, loginSchema } from "../zSchema/authSchema.js";
+import {
+  registerSchema,
+  loginSchema,
+  resetPassSchema,
+} from "../zSchema/authSchema.js";
 
 const router = Router();
 
@@ -12,5 +16,17 @@ router.post("/login", validate(loginSchema), authController.login);
 router.post("/refreshToken", authController.refreshToken);
 
 router.post("/logout", authController.logout);
+
+router.get("/verify-email/:token", authController.verifyEmail);
+
+router.post("/resend-verification", authController.resendVerification);
+
+router.post("/forgot-password", authController.forgotPassword);
+
+router.post(
+  "/reset-password",
+  validate(resetPassSchema),
+  authController.resetPassword,
+);
 
 export default router;
