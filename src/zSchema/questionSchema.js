124800be75a103c86d5f5a9444704b1b4dab6aee
ab-e1 <abelghy17@gmail.com>
2026-09-05 23:32:48 +0300

    fix: fixed the tag logic, and unhanded error

--- src/zSchema/questionSchema.js
+++ src/zSchema/questionSchema.js
@@ -6,5 +6,7 @@ export const questionSchema = z.object({
     .min(10, "question can not be empty, and canot be to less than 5 chracters")
     .max(200, "title too long continue the rest in the description"),
   description: z.string("description must be a string"),
-  tags: z.array(z.string().min(1, "atleast one tag is required")),
+  tags: z
+    .array(z.string().min(1, "tag cannot be empty"))
+    .min(1, "at least one tag is required"),
 });
