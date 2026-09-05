bd0ea5a83b9a9b9f75256cd5ecfa8bb350563db7
ab-e1 <abelghy17@gmail.com>
2026-09-05 23:33:37 +0300

    add: added a type in the sing in token.so there wont be cross using tokens for different ourpose, and started using one token verifier for all

--- src/utils/jwt.js
+++ src/utils/jwt.js
@@ -2,33 +2,29 @@ import jwt from "jsonwebtoken";
 import { jwtExpiresIn, jwtSecret } from "../config/loadEnv.js";
 
 export const signAccessToken = (payload) => {
-  return jwt.sign(payload, jwtSecret, {
+  return jwt.sign({ ...payload, type: "access" }, jwtSecret, {
     expiresIn: jwtExpiresIn,
   });
 };
 
-export const verifyAccessToken = (token) => {
-  return jwt.verify(token, jwtSecret);
-};
-
 export const signRefreshToken = (payload) => {
-  return jwt.sign(payload, jwtSecret, {
+  return jwt.sign({ ...payload, type: "refresh" }, jwtSecret, {
     expiresIn: "7d",
   });
 };
 
-export const verifyRefreshToken = (refreshToken) => {
-  return jwt.verify(refreshToken, jwtSecret);
-};
-
 export const signEmailVerficationToken = (payload) => {
-  return jwt.sign(payload, jwtSecret, {
+  return jwt.sign({ ...payload, type: "email_verification" }, jwtSecret, {
     expiresIn: "24h",
   });
 };
 
 export const signPasswordResetToken = (payload) => {
-  return jwt.sign(payload, jwtSecret, {
+  return jwt.sign({ ...payload, type: "password_reset" }, jwtSecret, {
     expiresIn: "15m",
   });
+};
+
+export const verifyToken = (token) => {
+  return jwt.verify(token, jwtSecret);
 };
