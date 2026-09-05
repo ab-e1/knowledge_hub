b30ccf9b72be91988fa99f98b56776cff878de3a
ab-e1 <abelghy17@gmail.com>
2026-09-05 23:31:57 +0300

    fix: fixed a typo error

--- src/zSchema/answerSchema.js
+++ src/zSchema/answerSchema.js
@@ -3,5 +3,5 @@ import { z } from "zod";
 export const answerSchema = z.object({
   content: z
     .string()
-    .min(2, "answer content can not be empty, and greater than 2 characters"),
+    .min(2, "answer must be at least 2 characters long"),
 });
