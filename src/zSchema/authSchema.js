d124113d8313c0b44b9fdb67332b4354d64ef447
ab-e1 <abelghy17@gmail.com>
2026-09-05 23:32:24 +0300

    fix: remover verbose redundunt code and replcaed it with a smaller one we can reuse

--- src/zSchema/authSchema.js
+++ src/zSchema/authSchema.js
@@ -2,25 +2,31 @@ import { z } from "zod";
 
 const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
+const passwordSchema = z
+  .string()
+  .min(8, "password must be atleast 8 characters long")
+  .regex(/[A-Z]/, "password must contain atleast one capital letter")
+  .regex(/[a-z]/, "password must contain atleast one small letter")
+  .regex(/\d/, "password must contain at least one number")
+  .regex(
+    /[!@#$%^&*+-_]/,
+    "password msut contain atleast one special character",
+  );
+
 export const registerSchema = z.object({
   name: z.string().min(1, "name can not be empty"),
   email: z
     .string()
     .min(1, "email can not be empty")
     .regex(emailRegex, "Invalid email"),
-  password: z
-    .string()
-    .min(8, "password must be atleast 8 characters long")
-    .regex(/[A-Z]/, "password must contain atleast one capital letter")
-    .regex(/[a-z]/, "password must contain atleast one small letter")
-    .regex(/\d/, "password must contain at least one number")
-    .regex(
-      /[!@#$%^&*+-_]/,
-      "password msut contain atleast one special character",
-    ),
+  password: passwordSchema,
 });
 
 export const loginSchema = z.object({
   email: z.string().min(1, "email cannot be empty"),
   password: z.string().min(1, "password can not be empty"),
+});
+
+export const resetPassSchema = z.object({
+  password: passwordSchema,
 });
