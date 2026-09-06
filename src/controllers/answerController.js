4a71f32589733c4b9cdf7efe5100f18971216f7b
ab-e1 <abelghy17@gmail.com>
2026-09-06 18:06:31 +0300

    add: added teh controller layer for the answers services

---
+++ src/controllers/answerController.js
@@ -0,0 +1,61 @@
+import * as answerService from "../services/answerService.js";
+import { failure, success } from "../utils/response.js";
+
+export const createAnswer = async (req, res, next) => {
+  try {
+    const result = await answerService.createAnswer(
+      Number(req.user.id),
+      Number(req.params.questionId),
+      req.body.content,
+    );
+    if (!result.ok) {
+      return failure(res, result.error, result.status);
+    }
+    return success(res, result.data, result.status);
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const getAnswers = async (req, res, next) => {
+  try {
+    const result = await answerService.getAnswers(Number(req.params.questionId));
+    if (!result.ok) {
+      return failure(res, result.error, result.status);
+    }
+    return success(res, result.data, result.status);
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const updateAnswer = async (req, res, next) => {
+  try {
+    const result = await answerService.updateAnswer(
+      Number(req.user.id),
+      Number(req.params.answerId),
+      req.body.content,
+    );
+    if (!result.ok) {
+      return failure(res, result.error, result.status);
+    }
+    return success(res, result.data, result.status);
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const deleteAnswer = async (req, res, next) => {
+  try {
+    const result = await answerService.deleteAnswer(
+      Number(req.user.id),
+      Number(req.params.answerId),
+    );
+    if (!result.ok) {
+      return failure(res, result.error, result.status);
+    }
+    return success(res, result.data, result.status);
+  } catch (err) {
+    next(err);
+  }
+};
