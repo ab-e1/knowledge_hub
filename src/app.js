d399f8c62c13b1bea4f066ff83c36ef753872137
ab-e1 <abelghy17@gmail.com>
2026-09-06 03:18:43 +0300

    add: added the question route to ath app.js so the app uses it

--- src/app.js
+++ src/app.js
@@ -1,7 +1,10 @@
 import express from "express";
 import authRoutes from "./routes/authRoute.js";
+import questionRoutes from "./routes/questionRoute.js";
 
 export const app = express();
 app.use(express.json());
 
 app.use("/api/v1/auth", authRoutes);
+
+app.use("/api/v1/questions", questionRoutes);
