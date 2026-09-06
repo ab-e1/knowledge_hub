8d501b9e5f33249953aea08be637a2f41707cd87
ab-e1 <abelghy17@gmail.com>
2026-09-06 17:58:43 +0300

    add: added the updated answer scheam for updated answers

--- src/zSchema/answerSchema.js
+++ src/zSchema/answerSchema.js
@@ -1,7 +1,7 @@
 import { z } from "zod";
 
 export const answerSchema = z.object({
-  content: z
-    .string()
-    .min(2, "answer must be at least 2 characters long"),
+  content: z.string().min(2, "answer must be at least 2 characters long"),
 });
+
+export const updateAnswerSchema = answerSchema.partial();
